1af3328169cec9cab1a62f0b909141057987b326
John Pursey <john.pursey@gmail.com>
2026-02-07 23:17:20 -0800

    Refactor jprsurf to build as a Windows DLL
    Replaced the executable target with a shared library in CMake.

--- src/jprsurf.cc
+++ src/jprsurf.cc
@@ -3,37 +3,39 @@
 // Use of this source code is governed by an MIT-style License that can be found
 // in the LICENSE file or at https://opensource.org/licenses/MIT.
 
-#include <iostream>
-#include <string_view>
-#include <vector>
+#include <windows.h>
 
-#include "absl/types/span.h"
+#include <string>
+
 #include "gb/base/context_builder.h"
 #include "gb/file/file_system.h"
 #include "gb/file/local_file_protocol.h"
 
 namespace jpr {
 
-bool Main(absl::Span<const std::string_view> args) {
-  if (args.size() != 1) {
-    std::cerr << "Usage: jprsurf <command> <args...>" << std::endl;
-    return false;
-  }
-
+void Attach() {
   gb::FileSystem file_system;
   file_system.Register(gb::LocalFileProtocol::Create(
       gb::ContextBuilder().SetValue<std::string>("root", "/").Build()));
   file_system.SetDefaultProtocol("file");
-
-  return true;
 }
 
 }  // namespace jpr
 
-int main(int argc, const char* argv[]) {
-  std::vector<std::string_view> args(argc - 1);
-  for (int i = 1; i < argc; ++i) {
-    args[i - 1] = argv[i];
-  }
-  return jpr::Main(args) ? 0 : -1;
+BOOL APIENTRY DllMain(HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved)
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        jpr::Attach();
+        break;
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        // Cleanup code goes here
+        break;
+    }
+    return TRUE;
 }
