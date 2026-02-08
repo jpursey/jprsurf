3c54f58ce8c6ad052433bf23ac61e98f1d70301d
John Pursey <john.pursey@gmail.com>
2026-02-08 01:09:46 -0800

    Remove gb library dependencies and file system setup
    Removed jpr::Attach() and all gb-related code from jprsurf.cc, including file system and protocol registration. Updated CMakeLists.txt to eliminate gb_file and gb_parse from dependencies. DllMain now contains only placeholder comments for initialization.

--- src/jprsurf.cc
+++ src/jprsurf.cc
@@ -5,23 +5,6 @@
 
 #include <windows.h>
 
-#include <string>
-
-#include "gb/base/context_builder.h"
-#include "gb/file/file_system.h"
-#include "gb/file/local_file_protocol.h"
-
-namespace jpr {
-
-void Attach() {
-  gb::FileSystem file_system;
-  file_system.Register(gb::LocalFileProtocol::Create(
-      gb::ContextBuilder().SetValue<std::string>("root", "/").Build()));
-  file_system.SetDefaultProtocol("file");
-}
-
-}  // namespace jpr
-
 BOOL APIENTRY DllMain(HMODULE hModule,
                        DWORD  ul_reason_for_call,
                        LPVOID lpReserved)
@@ -29,7 +12,7 @@ BOOL APIENTRY DllMain(HMODULE hModule,
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
-        jpr::Attach();
+        // One-time initialization code goes here
         break;
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
