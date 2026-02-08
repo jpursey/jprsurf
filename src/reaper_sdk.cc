5ae4fc77c612b5a8a20c9a410c3b5e760873d1dc
John Pursey <john.pursey@gmail.com>
2026-02-08 01:13:25 -0800

    Add 2026 copyright and MIT license header to source file
    Added copyright notice for 2026 (John Pursey) and MIT license statement with link at the top of reaper_sdk.cc. No functional code changes were made.

--- src/reaper_sdk.cc
+++ src/reaper_sdk.cc
@@ -1,2 +1,7 @@
+// Copyright (c) 2026 John Pursey
+//
+// Use of this source code is governed by an MIT-style License that can be found
+// in the LICENSE file or at https://opensource.org/licenses/MIT.
+
 #define REAPERAPI_IMPLEMENT
 #include "reaper_plugin_functions.h"
