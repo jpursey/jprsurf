b36ec9cec40e3c49d2ed5f352e2ab51414bdbbd6
John Pursey <john.pursey@gmail.com>
2026-02-22 08:55:17 -0800

    Add MIDI listener subscription system to MidiIn
    Introduced MidiMessage struct and MidiListener interface for handling MIDI input. MidiIn now supports subscribing/unsubscribing listeners for specific MIDI messages (by status and optionally data byte), and dispatches messages with precise timing.

--- src/midi_port.cc
+++ src/midi_port.cc
@@ -11,6 +11,18 @@
 
 namespace jpr {
 
+namespace {
+
+uint16_t MakeKey(uint8_t status, uint8_t data = 0x80) {
+  return (static_cast<uint16_t>(status) << 8) | data;
+}
+
+// This is the denominator used to convert MIDI time from the integer format
+// used by Reaper to seconds.
+constexpr double kSecondsPerMidiTick = 1.0 / 1024000.0;
+
+}  // namespace
+
 std::vector<std::unique_ptr<MidiIn>> MidiIn::GetPorts() {
   std::vector<std::unique_ptr<MidiIn>> ports;
   int port_count = GetNumMIDIInputs();
@@ -55,13 +67,79 @@ void MidiIn::Close() {
   LOG(INFO) << "Closed MIDI port " << index_ << " (" << name_ << ")";
 }
 
+void MidiIn::Subscribe(MidiListener* listener, uint8_t status) {
+  DoSubscribe(listener, MakeKey(status));
+}
+
+void MidiIn::Subscribe(MidiListener* listener, uint8_t status, uint8_t data) {
+  DoSubscribe(listener, MakeKey(status, data));
+}
+
+void MidiIn::DoSubscribe(MidiListener* listener, uint16_t key) {
+  listeners_[key].push_back(listener);
+  listener_keys_[listener].push_back(key);
+}
+
+void MidiIn::Unsubscribe(MidiListener* listener) {
+  auto it = listener_keys_.find(listener);
+  if (it == listener_keys_.end()) {
+    return;
+  }
+  for (uint16_t key : it->second) {
+    auto& vec = listeners_[key];
+    vec.erase(std::remove(vec.begin(), vec.end(), listener), vec.end());
+    if (vec.empty()) {
+      listeners_.erase(key);
+    }
+  }
+  listener_keys_.erase(it);
+}
+
 void MidiIn::Poll(const RunTime& time) {
   if (port_ == nullptr) {
     return;
   }
   port_->SwapBufsPrecise(time.coarse, time.precise);
 
-  // TODO: Process MIDI messages here.
+  auto* event_list = port_->GetReadBuf();
+  int bpos = 0;
+  while (true) {
+    MIDI_event_t* event = event_list->EnumItems(&bpos);
+    if (event == nullptr) {
+      break;
+    }
+
+    // Ignore sysex messages for now, since they can't be subscribed to.
+    if (event->midi_message[0] == 0xF0) {
+      continue;
+    }
+
+    // Convert to a 3-byte MIDI message, and time.
+    double message_time =
+        time.precise + event->frame_offset * kSecondsPerMidiTick;
+    MidiMessage message = {.status = event->midi_message[0],
+                           .data1 = event->midi_message[1],
+                           .data2 = event->midi_message[2]};
+
+    // First look up global listeners for this status byte, then look up
+    // listeners for this specific status and data byte.
+    if (auto it = listeners_.find(MakeKey(message.status));
+        it != listeners_.end()) {
+      for (MidiListener* listener : it->second) {
+        listener->OnMidiMessage(message_time, message);
+      }
+    }
+
+    // Now look up listeners for this specific status and data byte.
+    for (auto it = listeners_.find(MakeKey(message.status, message.data1));
+         it != listeners_.end() &&
+         it->first == MakeKey(message.status, message.data1);
+         ++it) {
+      for (MidiListener* listener : it->second) {
+        listener->OnMidiMessage(message_time, message);
+      }
+    }
+  }
 }
 
 }  // namespace jpr
--- src/midi_port.h
+++ src/midi_port.h
@@ -13,8 +13,37 @@
 
 namespace jpr {
 
+// Defines a standard MIDI message, which consists of a status byte and up to
+// two data bytes. Whether the data bytes are used depends on the status byte
+// (if they are not used they must be zero). This supports all MIDI messages
+// other than SysEx messages which are handled separately.
+struct MidiMessage {
+  uint8_t status;
+  uint8_t data1;
+  uint8_t data2;
+};
+
+class MidiListener {
+ public:
+  virtual ~MidiListener() = default;
+
+  // This is called when a MIDI message is received on the port that this
+  // listener is registered to. The time parameter is the time that the message
+  // was received, in seconds.
+  virtual void OnMidiMessage(double time, const MidiMessage& message) = 0;
+};
+;
+
+// This class represents a MIDI input port.
+//
+// It can be used to read MIDI messages from the port, and will automatically
+// poll for new messages when Open() is called, and stop polling when Close() is
+// called. The port will be automatically closed when the object is destroyed.
 class MidiIn {
  public:
+  // Enumerates the available MIDI input ports available in Reaper and returns a
+  // list of constructed MidiIn objects. The ports are not opened yet, so Open()
+  // must be called on any desired port.
   static std::vector<std::unique_ptr<MidiIn>> GetPorts();
 
   MidiIn(const MidiIn&) = delete;
@@ -27,8 +56,21 @@ class MidiIn {
   bool Open(RunRegistry& registry);
   void Close();
 
+  // Subscribes a listener to a specific MIDI message. If data is specified, the
+  // listener will only be called for messages with matching the first data
+  // byte. If they are not specified, the listener will be called for messages
+  // with any value with the specified status.
+  void Subscribe(MidiListener* listener, uint8_t status);
+  void Subscribe(MidiListener* listener, uint8_t status, uint8_t data);
+
+  // Unsubscribes a listener from all MIDI messages. The listener will no longer
+  // receive any MIDI messages from this port.
+  void Unsubscribe(MidiListener* listener);
+
  private:
   MidiIn(int index, std::string name) : index_(index), name_(std::move(name)) {}
+
+  void DoSubscribe(MidiListener* listener, uint16_t key);
 
   void Poll(const RunTime& time);
 
@@ -36,6 +78,8 @@ class MidiIn {
   std::string name_;
   midi_Input* port_ = nullptr;
   RunHandle run_handle_;
+  absl::flat_hash_map<uint16_t, std::vector<MidiListener*>> listeners_;
+  absl::flat_hash_map<MidiListener*, std::vector<uint16_t>> listener_keys_;
 };
 
 }  // namespace jpr
